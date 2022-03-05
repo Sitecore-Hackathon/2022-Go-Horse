e6da3d646286fbe008e4571a85fb03b908786df5
Rodrigo Peplau <rodrigo.peplau@altudo.co>
2022-03-05 16:38:37 -0300

    Refactor on the GraphQL Ext

--- graphql-ext/GoHorse.GraphQL.Ext/Providers/GoHorseSchemaProvider.cs
+++ graphql-ext/GoHorse.GraphQL.Ext/Providers/GoHorseSchemaProvider.cs
@@ -12,25 +12,16 @@ using Spe.Core.Host;
 
 namespace GoHorse.GraphQL.Ext.Providers
 {
-    /// <summary>
-    /// Sample of making your own schema provider
-    /// This sample enables you to query on the current context user
-    /// </summary>
-    public class WhoAmISchemaProvider : SchemaProviderBase
+    public class GoHorseSchemaProvider : SchemaProviderBase
     {
         public override IEnumerable<FieldType> CreateRootQueries()
         {
-            yield return new WhoAmIQuery();
+            yield return new GoHorseQuery();
         }
 
-        /// <summary>
-        /// Teaches GraphQL how to resolve the `whoAmI` root field.
-        ///
-        /// RootFieldType<UserGraphType, User> means this root field maps a `User` domain object into the `UserGraphType` graph type object.
-        /// </summary>
-        protected class WhoAmIQuery : RootFieldType<StringGraphType, IEnumerable<string>>
+        protected class GoHorseQuery : RootFieldType<StringGraphType, IEnumerable<string>>
         {
-            public WhoAmIQuery() : base(name: "whoAmI", description: "Gets the current user")
+            public GoHorseQuery() : base(name: "runCommand", description: "Runs a specific command")
             {
                 QueryArgument[] queryArgumentArray = new QueryArgument[1];
                 QueryArgument<StringGraphType> queryArgument = new QueryArgument<StringGraphType>();
@@ -83,25 +74,6 @@ namespace GoHorse.GraphQL.Ext.Providers
                 };
                 return ret;
                 
-            }
-        }
-
-        // because this graph type is referred to by the return type in the FieldType above, it is automatically
-        // registered with the schema. For implied types (e.g. interface implementations) you need to override CreateGraphTypes() and
-        // manually tell the schema they exist (because no graph type directly refers to those types)
-        protected class UserGraphType : ObjectGraphType<string>
-        {
-            public UserGraphType()
-            {
-                // graph type names must be unique within a schema, so if defining a multiple-schema-provider
-                // endpoint, ensure that you don't have name collisions between schema providers.
-                Name = "GoHorseReturn";
-                Field<NonNullGraphType<StringGraphType>>("name", resolve: context => context);
-                //Field<NonNullGraphType<BooleanGraphType>>("isAdministrator", resolve: context => context.Source.IsAdministrator);
-
-                // note that graph types can resolve other graph types; for example
-                // it would be possible to add a `lockedItems` field here that would
-                // resolve to an `Item[]` and map it onto `ListGraphType<ItemInterfaceGraphType>`
             }
         }
     }
