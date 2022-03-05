5220879f958740d3b08170ee02c92014a4282f43
Rodrigo Peplau <rodrigo.peplau@altudo.co>
2022-03-05 06:04:36 -0300

    SItecre CLI extension for Commands

---
+++ sitecore-cli/GoHorse.CLI.Command/RegisterExtension.cs
@@ -0,0 +1,37 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: Sitecore.DevEx.Extensibility.Publishing.RegisterExtension
+// Assembly: Sitecore.DevEx.Extensibility.Publishing, Version=4.1.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: 8AB531F3-111F-41B9-B846-117170FE660B
+// Assembly location: C:\Users\RodrigoPeplau\Desktop\Sitecore.DevEx.Extensibility.Publishing.4.1.1\plugin\Sitecore.DevEx.Extensibility.Publishing.dll
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Sitecore.Devex.Client.Cli.Extensibility;
+using Sitecore.Devex.Client.Cli.Extensibility.Subcommands;
+using GoHorse.CLI.Command.Commands;
+using Sitecore.DevEx.Serialization.Client;
+using System;
+using System.Collections.Generic;
+using System.CommandLine;
+
+namespace GoHorse.CLI.Command
+{
+    public class RegisterExtension : ISitecoreCliExtension
+    {
+        public IEnumerable<ISubcommand> AddCommands(IServiceProvider container)
+        {
+            PublishCommand requiredService = container.GetRequiredService<PublishCommand>();
+            ((System.CommandLine.Command)requiredService).AddCommand((System.CommandLine.Command)container.GetRequiredService<ListOfTargetsCommand>());
+            return (IEnumerable<ISubcommand>)new ISubcommand[1]
+            {
+        (ISubcommand) requiredService
+            };
+        }
+
+        public void AddConfiguration(IConfigurationBuilder configBuilder)
+        {
+        }
+
+        public void AddServices(IServiceCollection serviceCollection) => serviceCollection.AddSerialization().AddSingleton<PublishCommand>().AddSingleton<ListOfTargetsCommand>();
+    }
+}
---
+++ sitecore-cli/GoHorse.CLI.Command/Tasks/ListOfTargetsTask.cs
@@ -0,0 +1,66 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: Sitecore.DevEx.Extensibility.Publishing.Tasks.ListOfTargetsTask
+// Assembly: Sitecore.DevEx.Extensibility.Publishing, Version=4.1.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: 8AB531F3-111F-41B9-B846-117170FE660B
+// Assembly location: C:\Users\RodrigoPeplau\Desktop\Sitecore.DevEx.Extensibility.Publishing.4.1.1\plugin\Sitecore.DevEx.Extensibility.Publishing.dll
+
+using Microsoft.Extensions.Logging;
+using Sitecore.DevEx.Client.Logging;
+using Sitecore.DevEx.Client.Tasks;
+using Sitecore.DevEx.Configuration;
+using Sitecore.DevEx.Configuration.Models;
+using Sitecore.DevEx.Serialization.Client.Publishing;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GoHorse.CLI.Command.Tasks
+{
+    public class ListOfTargetsTask
+    {
+        private readonly IRootConfigurationManager _rootConfigurationManager;
+        private readonly ILogger _logger;
+        private readonly IContentPublisher _contentPublisher;
+
+        public ListOfTargetsTask(
+          IRootConfigurationManager rootConfigurationManager,
+          ILoggerFactory loggerFactory,
+          IContentPublisher contentPublisher)
+        {
+            this._rootConfigurationManager = rootConfigurationManager ?? throw new ArgumentNullException(nameof(rootConfigurationManager));
+            this._logger = (ILogger)loggerFactory.CreateLogger<PublishTask>();
+            this._contentPublisher = contentPublisher;
+        }
+
+        public async Task Execute(ListOfTargetsOptions options)
+        {
+            ((TaskOptionsBase)options).Validate();
+            EnvironmentConfiguration environmentConfiguration;
+            if (!(await this._rootConfigurationManager.ResolveRootConfiguration(options.Config)).Environments.TryGetValue(options.EnvironmentName, out environmentConfiguration))
+                throw new InvalidConfigurationException("Environment " + options.EnvironmentName + " was not defined. Use the login command to define it.");
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            List<string> list = (await this._contentPublisher.GetListOfTargetsAsync(environmentConfiguration).ConfigureAwait(false)).ToList<string>();
+            stopwatch.Stop();
+            this._logger.LogTrace(string.Format("Targets: Loaded in {0}ms ({1} targets).", (object)stopwatch.ElapsedMilliseconds, (object)list.Count));
+            if (list.Any<string>())
+            {
+                ColorLogExtensions.LogConsoleInformation(this._logger, "Targets list:", new ConsoleColor?(ConsoleColor.Yellow), new ConsoleColor?());
+                using (List<string>.Enumerator enumerator = list.GetEnumerator())
+                {
+                    while (enumerator.MoveNext())
+                        ColorLogExtensions.LogConsoleInformation(this._logger, enumerator.Current, new ConsoleColor?(ConsoleColor.Green), new ConsoleColor?());
+                    environmentConfiguration = (EnvironmentConfiguration)null;
+                    stopwatch = (Stopwatch)null;
+                }
+            }
+            else
+            {
+                ColorLogExtensions.LogConsoleInformation(this._logger, string.Format("Sitecore Cli didn't find any publishing targets on {0}", (object)environmentConfiguration.Host), new ConsoleColor?(ConsoleColor.Red), new ConsoleColor?());
+                environmentConfiguration = (EnvironmentConfiguration)null;
+                stopwatch = (Stopwatch)null;
+            }
+        }
+    }
+}
